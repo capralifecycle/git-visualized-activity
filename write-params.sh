453b010bf48171b6cd63e50a79569dceb650cce5
Henrik Steen <henrist@henrist.net>
2020-06-08 01:40:25 +0200

    Add script to store secret

---
+++ write-params.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu -o pipefail
+
+echo "Please be sure you are authed to the correct AWS account before continuing."
+
+printf "Enter GitHub token (input hidden): "
+read -r -s token
+echo
+
+if [ -z "$token" ]; then
+  echo "Aborting"
+  exit
+fi
+
+# TODO: Switch to Secrets Manager.
+# TODO: Tags.
+aws ssm put-parameter \
+  --name /git-visualized-activity-worker/prod/github-token \
+  --description "Value set by write-params.sh" \
+  --type SecureString \
+  --value "$token" \
+  --overwrite
+
+echo "OK"
