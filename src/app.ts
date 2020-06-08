3e620631a785b14a5f6453c60b1fa2fb5e7584c9
Henrik Steen <henrist@henrist.net>
2020-06-08 03:00:16 +0200

    Group values from external sources

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,31 @@ import { WebDeployStack } from "./web-deploy-stack"
 import { WebStack } from "./web-stack"
 import { WorkerStack } from "./worker-stack"
 
+// Values from external sources.
+// TODO: Dynamically resolve these so we better understand
+//  their source and can automate changes.
+const externalValues = {
+  // From https://github.com/capralifecycle/liflig-incubator-common-infra
+  vpcId: "vpc-0a67807e4aca6bb84",
+  // From buildtools setup.
+  jenkinsRoleArn:
+    "arn:aws:iam::923402097046:role/buildtools-jenkins-RoleJenkinsSlave-JQGYHR5WE6C5",
+  // From https://github.com/capralifecycle/liflig-incubator-common-infra
+  buildBucketName: "incub-common-build-artifacts-001112238813-eu-west-1",
+  // From https://github.com/capralifecycle/liflig-incubator-common-infra
+  buildEcrRepositoryArn:
+    "arn:aws:ecr:eu-west-1:001112238813:repository/incub-common-builds",
+  // From https://github.com/capralifecycle/liflig-incubator-common-infra
+  buildEcrRepositoryName: "incub-common-builds",
+}
+
 const incubatorEnv = {
   accountId: "001112238813",
   // Resources in us-east-1.
   cloudfront: {
     webBucketName: "incub-gva-web",
     region: "us-east-1",
+    // TODO: Update and mvoe to externalValues.
     certificateArn:
       "arn:aws:acm:us-east-1:923402097046:certificate/8c02e2fe-9399-4c51-8801-3c1af58eba1b",
   },
@@ -18,14 +37,8 @@ const incubatorEnv = {
   domainName: "gva.incubator.capra.tv",
   hostedZoneId: "TODO",
   region: "eu-west-1",
-  // From https://github.com/capralifecycle/liflig-incubator-common-infra
-  // TODO: It would be nice if we could resolve this dynamically.
-  vpcId: "vpc-0a67807e4aca6bb84",
+  vpcId: externalValues.vpcId,
 }
-
-const jenkinsRoleArn =
-  "arn:aws:iam::923402097046:role/buildtools-jenkins-RoleJenkinsSlave-JQGYHR5WE6C5"
-const buildBucketName = "incub-common-build-artifacts-001112238813-eu-west-1"
 
 const app = new cdk.App()
 tagResources(app, (stack) => ({
@@ -39,11 +52,11 @@ new WebDeployStack(app, `incub-gva-web-deploy`, {
     account: incubatorEnv.accountId,
     region: incubatorEnv.region,
   },
-  callerRoleArn: jenkinsRoleArn,
+  callerRoleArn: externalValues.jenkinsRoleArn,
   roleName: "incub-gva-jenkins",
   // TODO: Dynamically resolve.
   distributionId: incubatorEnv.distributionId,
-  buildsBucketName: buildBucketName,
+  buildsBucketName: externalValues.buildBucketName,
   webBucketName: incubatorEnv.cloudfront.webBucketName,
   resourcePrefix: "incub-gva",
 })
@@ -56,11 +69,8 @@ new WorkerStack(app, `incub-gva-worker`, {
   resourcePrefix: "incub-gva",
   vpcId: incubatorEnv.vpcId,
   webBucketName: incubatorEnv.cloudfront.webBucketName,
-  // TODO: Dynamically lookup.
-  ecrRepositoryArn:
-    "arn:aws:ecr:eu-west-1:001112238813:repository/incub-common-builds",
-  // TODO: Dynamically lookup.
-  ecrRepositoryName: "incub-common-builds",
+  ecrRepositoryArn: externalValues.buildEcrRepositoryArn,
+  ecrRepositoryName: externalValues.buildEcrRepositoryName,
   artifactStatus: new EcsUpdateImageArtifactStatus({
     artifactPushedAndTagUpdated: true,
   }),
