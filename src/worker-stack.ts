53f41c444ec562df214a84d7dac913be897541e7
Henrik Steen <henrist@henrist.net>
2020-06-21 03:22:01 +0200

    Use a lambda to trigger ecs task

--- src/worker-stack.ts
+++ src/worker-stack.ts
@@ -4,9 +4,13 @@ import * as ecs from "@aws-cdk/aws-ecs"
 import * as events from "@aws-cdk/aws-events"
 import * as targets from "@aws-cdk/aws-events-targets"
 import * as iam from "@aws-cdk/aws-iam"
+import * as lambda from "@aws-cdk/aws-lambda"
 import * as logs from "@aws-cdk/aws-logs"
 import * as s3 from "@aws-cdk/aws-s3"
+import * as ssm from "@aws-cdk/aws-ssm"
 import * as cdk from "@aws-cdk/core"
+import type { Handler } from "aws-lambda"
+import type * as _AWS from "aws-sdk"
 import { EcrAsset } from "./asset"
 import { WebStack } from "./web-stack"
 
@@ -101,19 +105,82 @@ export class WorkerStack extends cdk.Stack {
       vpc,
     })
 
+    // We use a lambda to execute the task, so that we can easily
+    // run the task outside the schedule if needed.
+
+    const launcher = new lambda.Function(this, "Launcher", {
+      environment: {
+        CLUSTER_NAME: cluster.clusterName,
+        SUBNETS: vpc.publicSubnets.map((it) => it.subnetId).join(","),
+        SECURITY_GROUPS: securityGroup.securityGroupName,
+        TASK_DEFINITION: taskDef.taskDefinitionArn,
+      },
+      handler: "index.handler",
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: new lambda.InlineCode(`exports.handler = ${runTask.toString()};`),
+      timeout: cdk.Duration.minutes(1),
+    })
+
+    launcher.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["ecs:RunTask"],
+        resources: [taskDef.taskDefinitionArn],
+        conditions: {
+          ArnEquals: {
+            "ecs:cluster": cluster.clusterArn,
+          },
+        },
+      }),
+    )
+
+    launcher.addToRolePolicy(
+      new iam.PolicyStatement({
+        actions: ["iam:PassRole"],
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        resources: [taskDef.taskRole.roleArn, taskDef.executionRole!.roleArn],
+      }),
+    )
+
+    new ssm.StringParameter(this, "LauncherParam", {
+      parameterName: `/cf/stack/${this.stackName}/launcher-fn-arn`,
+      stringValue: launcher.functionArn,
+    })
+
+    new cdk.CfnOutput(this, "LauncherArn", {
+      value: launcher.functionArn,
+    })
+
     const scheduleRule = new events.Rule(this, "ScheduleRule", {
       schedule: events.Schedule.expression("cron(0 4 * * ? *)"),
     })
 
-    scheduleRule.addTarget(
-      new targets.EcsTask({
-        cluster,
-        taskDefinition: taskDef,
-        securityGroup,
-        subnetSelection: {
-          subnetType: ec2.SubnetType.PUBLIC,
-        },
-      }),
-    )
+    scheduleRule.addTarget(new targets.LambdaFunction(launcher))
   }
+}
+
+const runTask: Handler = async () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const AWS = require("aws-sdk")
+  const ecs = new AWS.ECS() as _AWS.ECS
+
+  await ecs
+    .runTask({
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      cluster: process.env["CLUSTER_NAME"]!,
+      launchType: "FARGATE",
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      taskDefinition: process.env["TASK_DEFINITION"]!,
+      count: 1,
+      platformVersion: "LATEST",
+      networkConfiguration: {
+        awsvpcConfiguration: {
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          subnets: process.env["SUBNETS"]!.split(","),
+          // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+          securityGroups: process.env["SECURITY_GROUPS"]!.split(","),
+          assignPublicIp: "ENABLED",
+        },
+      },
+    })
+    .promise()
 }
