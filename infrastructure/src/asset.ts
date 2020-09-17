a084baabb2bbd9d3d0f75abddb69f15f7633285b
Henrik Steen <henrist@henrist.net>
2020-09-17 02:56:01 +0200

    Remove BuildMeta
    An attempt to simplify a bit. Might be reintroduced later.

--- infrastructure/src/asset.ts
+++ infrastructure/src/asset.ts
@@ -1,32 +1,16 @@
 import * as fs from "fs"
 import { isSnapshot } from "./utils"
 
-export interface BuildMeta {
-  buildTimestamp: string
-  buildGitCommit: string
-  buildGitBranch: string
-  buildNr: string
-  buildTag: string
-}
-
 export interface EcrAsset {
   dockerTag: string
   ecrRepoArn: string
   ecrRepoName: string
-  meta: BuildMeta
 }
 
 const ecrAssetSnapshot: EcrAsset = {
   dockerTag: "dummy",
   ecrRepoArn: "arn:aws:ecr:eu-west-1:123456789012:repository/dummy",
   ecrRepoName: "dummy",
-  meta: {
-    buildTimestamp: "2019-12-16T12:00:00+00:00",
-    buildGitCommit: "dummy",
-    buildGitBranch: "dummy",
-    buildNr: "0",
-    buildTag: "dummy",
-  },
 }
 
 export function getEcrAsset(name: string): EcrAsset {
